51d723570284f31776ff513df3512fc9841319c0
Altair Dias <89491054+altairfd@users.noreply.github.com>
2023-04-25 19:43:21 -0300

    Add files via upload

---
+++ lista04b/ex07/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+
+    printf("Digite a altura do triângulo: ");
+    scanf("%d", &n);
+
+    for (i = n; i >= 1; i--) {
+        for (j = 1; j <= i; j++) {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
